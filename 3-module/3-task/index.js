cc4615dafd5209567cc080fddab2254f7e236f82
Valery Mashenets <webvalery.git@gmail.com>
2022-03-22 11:26:23 +0300

    Занятие 3: Функции и массивы

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,13 @@
 function camelize(str) {
-  // ваш код...
+  let result = '';
+  strDevide = str.split('');
+  for(let i = 0; i < strDevide.length; i++) {
+    if(strDevide[i] == '-') {
+      strDevide[i + 1] = strDevide[i + 1].toUpperCase();
+      strDevide.splice(i, 1);
+    } 
+  }
+  result = strDevide.join('');
+
+  return result;
 }
