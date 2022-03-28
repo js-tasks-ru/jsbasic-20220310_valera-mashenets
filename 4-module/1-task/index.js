36fa19a7e94a24c126339c2e249c6dfc515bd9cb
Valery Mashenets <webvalery.git@gmail.com>
2022-03-28 15:22:33 +0300

    Занятие 4: Знакомство с DOM

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,4 +1,10 @@
 function makeFriendsList(friends) {
-  // ваш код...
-  console.log('привет');
+  let result = '';
+  let ul = document.createElement('ul');
+  for(let i = 0; i < friends.length; i++) {
+    result += `<li>${friends[i].firstName} ${friends[i].lastName}</li>`;
+  }
+  ul.innerHTML = result;
+  return ul;
 }
+
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,22 @@
 function highlight(table) {
-  // ваш код...
+  let tbody = table.querySelector('tbody');
+  for (let row of tbody.rows) {
+    if (row.lastElementChild.getAttribute('data-available') === 'true') {
+      row.classList.add('available');
+    } else if (row.lastElementChild.getAttribute('data-available') === 'false') {
+      row.classList.add('unavailable');
+    } else if (row.lastElementChild.getAttribute('data-available') === null) {
+      row.setAttribute('hidden','');
+    }
+
+    if(row.cells[2].innerHTML === 'm') {
+      row.classList.add('male');
+    } else if (row.cells[2].innerHTML === 'f') {
+      row.classList.add('female');
+    }
+
+    if(+row.cells[1].innerHTML < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+  }
 }
