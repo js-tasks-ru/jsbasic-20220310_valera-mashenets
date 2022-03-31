84bb5f011a5c9b023f2a25b276f75007d4f5e7d6
Valery Mashenets <webvalery.git@gmail.com>
2022-04-01 00:57:31 +0300

    Занятие 5: Обработчики событий

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,30 @@
 function initCarousel() {
-  // ваш код...
+  let screenSlides = document.querySelector('.carousel__inner');
+  let allSlides = document.querySelectorAll('.carousel__slide');
+  let widthOneSlide = document.querySelector('.carousel__slide').offsetWidth;
+  let carouselArrowLeft = document.querySelector('.carousel__arrow_left');
+  let carouselArrowRight = document.querySelector('.carousel__arrow_right');
+  let valueTranslateX = 0;
+  
+  function checkArrow() {
+    (valueTranslateX === 0) ?
+      carouselArrowLeft.style.display = 'none' : carouselArrowLeft.style.display = '';
+  
+    (valueTranslateX === -(widthOneSlide * allSlides.length - widthOneSlide)) ?
+      carouselArrowRight.style.display = 'none' : carouselArrowRight.style.display = '';
+  }
+
+  carouselArrowLeft.addEventListener('click', () => {
+    valueTranslateX += widthOneSlide;
+    screenSlides.style.transform = `translateX(${valueTranslateX}px)`;
+    checkArrow()
+  });
+
+  carouselArrowRight.addEventListener('click', () => {
+    valueTranslateX -= widthOneSlide;
+    screenSlides.style.transform = `translateX(${valueTranslateX}px)`;
+    checkArrow()
+  });
+  
+  checkArrow();
 }
