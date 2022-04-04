59e7fdeff2b63092fa35a320863f85decccbcf6a
Valery Mashenets <webvalery.git@gmail.com>
2022-04-05 01:04:50 +0300

    Занятие 6: JS классы и компоненты Задание 2

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,36 @@
+import createElement from '../../assets/lib/create-element.js';
 export default class ProductCard {
   constructor(product) {
+    this.elem = this.createCard(product);
+    this.handler(this.elem, product);
+  }
+
+  createCard(product) {
+    let dom = createElement(`
+    <div class="card">
+      <div class="card__top">
+          <img src="/assets/images/products/${product.image}" class="card__image" alt="product">
+          <span class="card__price">€${product.price.toFixed(2)}</span>
+      </div>
+      <div class="card__body">
+          <div class="card__title">${product.name}</div>
+          <button type="button" class="card__button">
+              <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+      </div>
+    </div>
+    `);
+    return dom;
+  }
+
+  handler(dom, product) {
+    let btn = dom.querySelector('.card__button');
+    btn.addEventListener('click', (event) => {
+      let genEvent = new CustomEvent("product-add", {
+        detail: product.id,
+        bubbles: true
+      });
+      dom.dispatchEvent(genEvent);
+    });
   }
 }
