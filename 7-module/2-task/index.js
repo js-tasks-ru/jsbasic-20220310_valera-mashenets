236b67a8eaa9f25ae1b3705b3a8d34a572c5060d
Valery Mashenets <webvalery.git@gmail.com>
2022-04-11 21:09:04 +0300

    Занятие 7: Сложные компоненты

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,5 +2,61 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    this.modal = this.createTmpl();
+    this.keyBind = this.key.bind(this);
+  }
+
+  createTmpl() {
+    let tmpl = createElement(`
+    <div class="modal">
+			<div class="modal__overlay"></div>
+			<div class="modal__inner">
+			<div class="modal__header">
+				<button type="button" class="modal__close">
+				<img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+				</button>
+				<h3 class="modal__title"></h3>
+			</div>
+			<div class="modal__body"></div>
+			</div>
+		</div>`);
+
+    return tmpl;
+  }
+
+  open() {
+    if (!document.querySelector('.modal')) {
+      document.body.classList.add('is-modal-open');
+      document.body.append(this.modal);
+      this.handlers();
+    }
+  }
+
+  close() {
+    if (document.querySelector('.modal')) {
+      document.body.classList.remove('is-modal-open');
+      document.querySelector('.modal').remove();
+      document.removeEventListener('keydown', this.keyBind);
+    }
+  }
+
+  setTitle(title) {
+    this.modal.querySelector('.modal__title').textContent = title;
+  }
+
+  setBody(node) {
+    this.modal.querySelector('.modal__body').innerHTML = '';
+    this.modal.querySelector('.modal__body').appendChild(node);
+  }
+
+  key(event) {
+    if (event.code === 'Escape') {
+      this.close();
+    }
+  }
+
+  handlers() {
+    document.addEventListener('keydown', this.keyBind);
+    this.modal.querySelector('.modal__close').addEventListener('click', this.close);
   }
 }
--- 7-module/4-task/index.js
+++ 7-module/4-task/index.js
@@ -1,4 +1,120 @@
+import createElement from "../../assets/lib/create-element.js";
+
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.config = {
+      steps,
+      value,
+    };
+    this.elem = this.render();
+    this.handlers();
+    this.drag();
+  }
+
+  render() {
+    const tmpl = createElement(`
+      <div class="slider">
+        <div class="slider__thumb" style="left: 50%;">
+          <span class="slider__value">2</span>
+        </div>
+        <div class="slider__progress" style="width: 50%;"></div>
+        <div class="slider__steps">
+        </div>
+      </div>`);
+    let sliderSteps = tmpl.querySelector('.slider__steps');
+    for (let i = 0; i < this.config.steps; i++) {
+      let spanGen;
+      if (i === 0) {
+        spanGen = createElement(`<span class="slider__step-active"></span>`);
+      } else {
+        spanGen = createElement(`<span></span>`);
+      }
+      sliderSteps.appendChild(spanGen);
+    }
+
+    return tmpl;
+  }
+
+  handlers() {
+    this.elem.addEventListener('click', (event) => {
+      let sliderSpans = this.elem.querySelector('.slider__steps');
+      let sliders = sliderSpans.querySelectorAll('span');
+      for (let item of sliders) {
+        if (item.classList.contains('slider__step-active')) {
+          item.classList.remove('slider__step-active');
+        }
+      }
+
+      let xClick = event.clientX - this.elem.getBoundingClientRect().left;
+      let xClickRelative = xClick / this.elem.offsetWidth;
+      let value = Math.round(xClickRelative * (this.config.steps - 1));
+      let valuePercent = value / (this.config.steps - 1) * 100;
+
+      this.elem.querySelector('.slider__value').textContent = value;
+      sliderSpans.querySelectorAll('span')[value].classList.add('slider__step-active');
+
+      let sliderThumb = this.elem.querySelector('.slider__thumb');
+      let sliderProgress = this.elem.querySelector('.slider__progress');
+      sliderThumb.style.left = `${valuePercent}%`;
+      sliderProgress.style.width = `${valuePercent}%`;
+
+      let objectEvent = new CustomEvent('slider-change', {
+        detail: value,
+        bubbles: true
+      });
+      this.elem.dispatchEvent(objectEvent);
+    });
+  }
+
+  drag() {
+    let sliderThumb = this.elem.querySelector('.slider__thumb');
+    let sliderProgress = this.elem.querySelector('.slider__progress');
+    let slider = this.elem;
+    let thisConfig = this.config;
+    let value;
+    sliderThumb.ondragstart = () => false;
+
+    sliderThumb.onpointerdown = function (event) {
+      slider.classList.add('slider_dragging');
+      function moveAt(event) {
+        let pos = event.clientX - slider.getBoundingClientRect().left;
+        let posRelative = pos / slider.offsetWidth;
+
+        if (posRelative < 0) {
+          posRelative = 0;
+        }
+        if (posRelative > 1) {
+          posRelative = 1;
+        }
+        let posPercent = posRelative * 100;
+        sliderThumb.style.left = `${posPercent}%`;
+        sliderProgress.style.width = `${posPercent}%`;
+
+        value = Math.round(posRelative * (thisConfig.steps - 1));
+
+        let slidersSteps = slider.querySelector('.slider__steps');
+        let slidersAll = slidersSteps.querySelectorAll('span');
+        for(let item of slidersAll) {
+          item.classList.remove('slider__step-active');
+        }
+        slidersAll[value].classList.add('slider__step-active');
+        slider.querySelector('.slider__value').textContent = value;
+      }
+      
+      document.addEventListener('pointermove', moveAt);
+
+      document.addEventListener('pointerup', function () {
+        let valueEnd = value / (thisConfig.steps - 1) * 100;
+        sliderThumb.style.left = `${valueEnd}%`;
+        sliderProgress.style.width = `${valueEnd}%`;
+        document.removeEventListener('pointermove', moveAt);
+        slider.classList.remove('slider_dragging');
+        let eventCastom = new CustomEvent('slider-change', { 
+          detail: value, 
+          bubbles: true
+        });
+        slider.dispatchEvent(eventCastom);
+      });
+    }
   }
 }
