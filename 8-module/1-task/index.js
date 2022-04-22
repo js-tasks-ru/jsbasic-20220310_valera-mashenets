0a41e8331f1149723a6b5fb6d12f63de75891f02
Valery Mashenets <webvalery.git@gmail.com>
2022-04-22 23:32:01 +0300

    Занятие 8: Взаимодействие с сервером

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -21,12 +21,14 @@ export default class CartIcon {
           <span class="cart-icon__price">€${cart.getTotalPrice().toFixed(2)}</span>
         </div>`;
 
+
+      this.coordY = this.elem.getBoundingClientRect().top + window.pageYOffset;
       this.updatePosition();
 
       this.elem.classList.add('shake');
       this.elem.addEventListener('transitionend', () => {
         this.elem.classList.remove('shake');
-      }, {once: true});
+      }, { once: true });
 
     } else {
       this.elem.classList.remove('cart-icon_visible');
@@ -39,6 +41,23 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    // ваш код ...
+    if (this.elem.offsetWidth > 0) {
+      let leftIndent = Math.min(
+        document.querySelector('.container').getBoundingClientRect().right + 20,
+        document.documentElement.clientWidth - this.elem.offsetWidth - 10
+      ) + 'px';
+
+      if (window.pageYOffset > this.coordY) {
+        this.elem.style.position = 'fixed';
+        this.elem.style.top = '50px';
+        this.elem.style.left = `${leftIndent}`;
+        this.elem.style.zIndex = 1000;
+      } else {
+        this.elem.style.position = '';
+        this.elem.style.top = '';
+        this.elem.style.left = '';
+        this.elem.style.zIndex = '';
+      }
+    }
   }
 }
--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,74 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.tempProd = [];
+    this.elem = this.render();
+  }
+
+  render() {
+    let htmlTmpl = createElement(`
+    <div class="products-grid">
+      <div class="products-grid__inner">
+      </div>
+    </div>
+    `);
+    let productInner = htmlTmpl.querySelector('.products-grid__inner');
+
+    let isEmptyFilters = (() => {
+      for (let item in this.filters) {
+        return false;
+      }
+      return true;
+    })();
+
+
+    if (isEmptyFilters) {
+      for (let i = 0; i < this.products.length; i++) {
+        let cardItem = new ProductCard(this.products[i]);
+        productInner.append(cardItem.elem);
+      }
+    }
+    return htmlTmpl;
+  }
+
+  updateFilter(filters) {
+    for (let item in filters) {
+      this.filters[item] = filters[item];
+    }
+
+    this.tempProd = [];
+    for (let i = 0; i < this.products.length; i++) {
+
+      if (this.products[i].nuts === true && this.filters.noNuts === true) {
+        continue;
+      }
+
+      if (this.filters.vegeterianOnly === true) {
+        if (this.products[i].vegeterian === true) {
+        } else continue;
+      }
+
+      if (this.products[i].spiciness > this.filters.maxSpiciness) {
+        continue;
+      }
+
+      if (('category' in this.filters) && this.filters.category !== '') {
+        if (this.products[i].category === this.filters.category) {
+        } else continue;
+      }
+
+      this.tempProd.push(this.products[i]);
+    }
+    let prodInner = document.querySelector('.products-grid__inner');
+    let cardsOld = prodInner.querySelectorAll('.card');
+    for (let i = 0; i < cardsOld.length; i++) {
+      cardsOld[i].remove();
+    }
+
+    for (let i = 0; i < this.tempProd.length; i++) {
+      let cardItem = new ProductCard(this.tempProd[i]);
+      prodInner.append(cardItem.elem);
+    }
   }
 }
+
